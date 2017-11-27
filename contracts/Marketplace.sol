39ed6e8f3097d6b0fc481ac6971771a6c5128fca
kwanhongyap <kwanhongyap@berkeley.edu>
2017-11-26 17:14:08 -0800

    contract files added

---
+++ contracts/Marketplace.sol
@@ -0,0 +1,12 @@
+pragma solidity ^0.4.15;
+
+import './Renter.sol';
+import './Owner.sol';
+import './Transaction.sol';
+import './Request.sol';
+import './Rentpayment.sol';
+import './Rentdeposit.sol';
+
+contract Marketplace {
+    
+}
