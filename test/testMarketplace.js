d42cce3e532c7ede21b43c963f4f9a65861fd7e6
kwanhongyap <kwanhongyap@berkeley.edu>
2017-11-26 18:22:23 -0800

    fixed test marketplace

--- test/testMarketplace.js
+++ test/testMarketplace.js
@@ -6,7 +6,7 @@ const Marketplace = artifacts.require("./Marketplace.sol");
 contract('TestMarketplace', function(accounts) {
 	
     const owner1 = accounts[0];
-    print(owner1)
+    //print(owner1)
 	const owner2 = accounts[1];
 	let marketplace;
 	
