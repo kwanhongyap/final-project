78692dd619f391c7bb377064fd9229f34e0e8010
kwanhongyap <kwanhongyap@berkeley.edu>
2017-11-26 19:25:37 -0800

    fixed marketplace

--- contracts/Marketplace.sol
+++ contracts/Marketplace.sol
@@ -9,7 +9,7 @@ import './Rentdeposit.sol';
 
 contract Marketplace {
     address public owner;
-    uint public temp;
+    //uint public temp;
     Item[] public itemlist;
 	Renter rentstruct;
 	Owner ownstruct;
@@ -28,13 +28,13 @@ contract Marketplace {
         //initial constructor
         owner = msg.sender;
         //Item[] memory itemlist = new Item[](1);
-        temp = 0;
+        //temp = 0;
     }
 
     function upLoad(bytes32 name, uint price) {
         //owner upload items to marketplace contract
         address owner = msg.sender;
-        temp = itemlist.length;
+        //temp = itemlist.length;
         Item memory uitem = Item(name, owner, price);
         itemlist.push(uitem);
     }
